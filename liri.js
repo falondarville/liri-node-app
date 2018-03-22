a58a2c1dfc849692f5105c0b7b88667f2efa1238
Falon Darville <falondarville@gmail.com>
2018-03-21 20:06:47 -0700

    OMDB

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ var keys = require("./keys.js");
 //require apis
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
+var request = require("request");
 
 //access key information
 //Spotify
@@ -21,10 +22,15 @@ var client = new Twitter({
 	access_token_secret: keys.twitter.access_token_secret
 });
 
-client.post("statuses/update", {status: "I am not a bot"}, function(error, tweet, response) {
-	if(error) throw "Unable to post information";
-	console.log(tweet);
-	console.log(response);
+var params = {screen_name: 'nodejs', count: 20};
+
+client.get("statuses/user_timeline", params, function(error, tweet, response) {
+
+	if(!error) {
+		console.log(tweet);
+	} else {
+		console.log(error);
+	}
 });
 
 // spotify.search({type: "track", query: "All the Small Things"}, function(err, data) {
@@ -34,15 +40,34 @@ client.post("statuses/update", {status: "I am not a bot"}, function(error, tweet
 // 	console.log(data);
 // })
 
-//commands that liri will understand 
-
 //my-tweets will show the last 20 tweets and when they were created
 
 //spotify-this-song "name of song" will show artist, song name, preview link to the song on Spotify, album
 
 //default song is "The Sign" by Ace of Base
 
-//movie-this "movie name " will show title of movie, year, IMDB rating, Rotten Tomatoes rating, country where produced, language of movie, plot, actors
+//OMDB request
+if (process.argv[2] == "movie-this") {
 
-//default movie is "Mr. Nobody"
+	var movieTitle = process.argv[3] || "Mr. Nobody";
 
+	request("http://www.omdbapi.com/?apikey=trilogy&t=" + movieTitle, function(error, response, body) {
+
+	if(!error && response.statusCode === 200) {
+
+		var responseJson = JSON.parse(body);
+
+		var movie = responseJson.Title;
+		var year = responseJson.Year;
+		var IMDBrating = responseJson.Ratings[0].Source;
+		var RTrating = responseJson.Ratings[1].Source;
+		var country = responseJson.Country;
+		var language = responseJson.Language;
+		var plot = responseJson.Plot;
+		var actors = responseJson.Actors;
+
+		console.log("Movie title: " + movie + "\n" + "Year: " + year + "\n" + "IMBD Rating: " + IMDBrating + "\n" + "Rotten Tomatoes Rating: " + RTrating + "\n" + "Country: " + country + "\n" + "Language: " + language + "\n" + "Plot: " + plot + "\n" + "Actors: " + actors);
+	}
+});
+
+} 
